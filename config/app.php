cbb4542b074659fc212344088718037204f6cce2
Pete Houston <contact@petehouston.com>
2015-05-07 11:58:36 +0700

    Register HTML, Form.

--- config/app.php
+++ config/app.php
@@ -135,6 +135,7 @@ return [
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
+		'Illuminate\Html\HtmlServiceProvider',
 
 		/*
 		 * Application Service Providers...
@@ -143,7 +144,7 @@ return [
 		'App\Providers\BusServiceProvider',
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
-        'App\Providers\RouteServiceProvider',
+		'App\Providers\RouteServiceProvider',
 		'App\Providers\ConsoleCommandServiceProvider',
 
 	],
@@ -193,6 +194,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+		'Form'      => 'Illuminate\Html\FormFacade',
+		'HTML'      => 'Illuminate\Html\HtmlFacade',
 
 	],
 
